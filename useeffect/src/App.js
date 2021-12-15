6560aaeee907d075458d059b916f41d6f108a986
sagarpatil860 <sagarpatil860@gmail.com>
2021-12-15 13:46:37 +0530

    no message

---
+++ useeffect/src/App.js
@@ -0,0 +1,16 @@
+
+import { useState,useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const[count,setcount]=useState(0);
+ useEffect(()=>{setTimeout(()=>setcount(count+1),500); });
+  return (
+  <><h1>{count}</h1>
+  <button type="button" onClick={()=>setcount(count+1) }>Click Me!</button>
+  </>
+  );
+  
+}
+
+export default App;
